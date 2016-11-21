0a60bfe1adb98332e53fcc2764811d4eba09456e
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-11-21 03:31:21 +0100

    Added photo Upload

--- PhotoExploration/Areas/Administration/Controllers/ManagementController.cs
+++ PhotoExploration/Areas/Administration/Controllers/ManagementController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using PhotoExploration.Areas.Administration.Models;
+using PhotoExploration.Models;
 
 namespace PhotoExploration.Areas.Administration.Controllers
 {
@@ -20,6 +24,64 @@ namespace PhotoExploration.Areas.Administration.Controllers
                 var comments = db.Comments.Include(i => i.Photo).Where(x => x.UserId == userId);
 
                 return View(comments.ToList());
+            }
+        }
+
+        [Authorize]
+        public ActionResult UploadPhoto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult UploadPhoto(UploadPhotoModel model, HttpPostedFileBase photo)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            using (var db = new PhotoExplorationContext())
+            {
+                db.Photos.Add(new Photo
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    DateAdded = DateTime.Now,
+                    Path = photo.FileName
+                });
+
+                db.SaveChanges();
+            }
+
+            photo.SaveAs(Path.Combine(Server.MapPath("~/Photos"), photo.FileName));
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult DeleteComment(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            using (var db = new PhotoExplorationContext())
+            {
+                var comment = db.Comments.Include(i => i.Photo).FirstOrDefault(x => x.Id == id);
+                return comment == null ? (ActionResult) HttpNotFound() : View(comment);
+            }
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("DeleteComment")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCommentConfirmed(int id)
+        {
+            using (var db = new PhotoExplorationContext())
+            {
+                var comment = db.Comments.Include(i => i.Photo).FirstOrDefault(x => x.Id == id);
+                db.Comments.Remove(comment);
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
             }
         }
     }
